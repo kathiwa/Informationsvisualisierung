aaf5ad4cc5edcab3a763966267ec6539e84db8c3
kathiwa <katharina906@gmail.com>
2019-04-29 14:38:11 +0200

    Update

--- 20190428_TidyTuesday_Anime/app.R
+++ 20190428_TidyTuesday_Anime/app.R
@@ -9,51 +9,49 @@
 
 library(shiny)
 library(tidyverse)
-library(readxl)
-
-bond_movies <- read_delim("bond.csv", delim = ";")
+library(lubridate)
 
 
+tidy_anime <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-23/tidy_anime.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Bond Movie Data"),
+   titlePanel("Anime Dataset"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("like",
-                     "Wie gut gefallen dir James Bond Filme?",
-                     min = -10,
-                     max = 10,
-                     value = 5)
+         sliderInput("favorites",
+                     "Number of bins:",
+                     min = 1,
+                     max = 100,
+                     value = 20)
       ),
-     # radioButtons(inputId = "actor",
-      #               label = "Welcher ist dein Lieblingsschauspieler?",
-       #              choices = c("Connery","Lazenby", "Moore","Dalton","Brosnan","Craig")
-      #),
+      
       # Show a plot of the generated distribution
       mainPanel(
          plotOutput("distPlot")
       )
-   )
-)
+    )
+  )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
- #  output$distPlot <- renderPlot({
+   output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-  #    x    <- faithful[, 2] 
-   #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins +1)
       
       # draw the histogram with the specified number of bins
-    #  hist(x, breaks = bins, col = 'darkgray', border = 'white')
- #  })
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
 
--- Test/app.R
+++ Test/app.R
@@ -8,32 +8,23 @@
 #
 
 library(shiny)
-library(tidyverse)
-library(readxl)
-
-bond_movies <- read_delim("bond.csv", delim = ";")
-
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Bond Movie Data"),
+   titlePanel("Old Faithful Geyser Data"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("like",
-                     "Wie gut gefallen dir James Bond Filme?",
-                     min = -10,
-                     max = 10,
-                     value = 5)
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
       ),
-     # radioButtons(inputId = "actor",
-      #               label = "Welcher ist dein Lieblingsschauspieler?",
-       #              choices = c("Connery","Lazenby", "Moore","Dalton","Brosnan","Craig")
-      #),
+      
       # Show a plot of the generated distribution
       mainPanel(
          plotOutput("distPlot")
@@ -44,14 +35,14 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
- #  output$distPlot <- renderPlot({
+   output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-  #    x    <- faithful[, 2] 
-   #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
-    #  hist(x, breaks = bins, col = 'darkgray', border = 'white')
- #  })
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
 }
 
 # Run the application 
